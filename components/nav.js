a6669b11da5f24d3d5a3c0007fa7c20bc7408f99
ilci66 <akbiyik.ilker7@gmail.com>
2021-09-21 16:59:08 +0300

    nav component created

---
+++ components/nav.js
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+
+// gonna create later
+// import styles from '../styles/Nav.module.css';
+
+export default function Nav() {
+    return (
+        <nav className={styles.nav}>
+            <ul className={styles.list}>
+                <li className={styles.item}>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/add-post">
+                        <a>Add post</a>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+// importing it this way applied the css in that file to all the present pages
+// it is convention to import it here
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
